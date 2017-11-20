45302a0b89978ac2833798b3ac8d6ea4adc71d9a
ironsalsa <ironsalsa@gmail.com>
2017-11-20 12:55:02 -0700

    Typo

--- scripts/bundleup.sh
+++ scripts/bundleup.sh
@@ -1,4 +1,4 @@
-#!/bin/bash
+#! /bin/bash
 # This bundles up PM2 for install on an offline system - see https://stackoverflow.com/questions/41156556/what-exact-command-is-to-install-pm2-on-offline-rhel 
 # Also see https://www.npmjs.com/package/npm-bundle 
 
